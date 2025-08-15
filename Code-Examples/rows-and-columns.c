6818a4312c4135ba6d699016316b89e361646154
Ibrahim Shukurov <ibrahimshukurov275@gmail.com>
2025-08-15 13:14:25 +0400

    Rows and Columns with For loop

---
+++ Code-Examples/rows-and-columns.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+
+    int rows = 0;
+    int columns = 0;
+    char symbol = '\0';
+
+    printf("Enter the # of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the # of columns: ");
+    scanf("%d", &columns);
+    printf("Enter a symbol to use: ");
+    scanf(" %c", &symbol);
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++) {
+            printf("%c", symbol);
+        }
+
+        printf("\n");
+    }
+
+
+    return 0;
+}
